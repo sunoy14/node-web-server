2b6301b6d043fabb0d11d9f95e40f7ca4a1d3d3d
Millo Lailang <sunoy14socmed@gmail.com>
2018-01-21 09:56:19 +0530

    made changes

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+	//res.send('<h1>Hello Express!</h1>');
+	res.send({
+		name: 'Millo Lailang',
+		likes: [
+			'Musical instruments', 
+			'chess'
+		]
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		errorMessage: 'Unable to handle request'
+	});
+});
+
+app.listen(3000, () => console.log('app listening on port 3000'));
