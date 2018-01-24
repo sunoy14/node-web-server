780f5a04e98ba8f2c32057ba86271f8202a2afef
Millo Lailang <sunoy14socmed@gmail.com>
2018-01-24 15:12:55 +0530

    made changes

--- server.js
+++ server.js
@@ -1,22 +1,51 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if(err){
+			console.log(err);
+		}
+	});
+	next();	
+});
+
+//app.use((req, res, next) => {
+//	res.render('maintenance',{
+//		pageTitle: 'Maintenance Page'
+//	});
+//});
+
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase(text);
+});
+
 app.get('/', (req, res) => {
-	//res.send('<h1>Hello Express!</h1>');
-	res.send({
-		name: 'Millo Lailang',
-		likes: [
-			'Musical instruments', 
-			'chess'
-		]
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Welcome',
 	});
 });
 
 app.get('/about', (req, res) => {
-	res.send('About Page');
+	res.render('about.hbs', {
+		pageTitle: 'About Page',
+	});
 });
 
 app.get('/bad', (req, res) => {
